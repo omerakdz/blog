b65ac0289a2d8408185b78ce67dae12c86454b26
Ömer Akdeniz <s149155@ap.be>
2026-03-02 12:39:31 +0100

    adjustments of weekly reports

--- src/app/services/blog.service.ts
+++ src/app/services/blog.service.ts
@@ -9,37 +9,38 @@ export class BlogService {
   private posts: BlogPost[] = [
     {
       id: 1,
-      title: 'Eerste week van mijn stage',
+      title: 'Kennismaking en oriëntatie',
       date: new Date('2026-02-02'),
       content:
         'De eerste week van mijn stage was vooral gericht op kennismaking en oriëntatie. Ik kreeg een uitgebreide uitleg over het project, de werkwijzen en de technologieën die we gebruiken. Thuis heb ik de projectdocumentatie doorgenomen en nagedacht over mogelijke aanpakken. Vervolgens maakte ik een kennismakingsoefening die ik samen met mijn mentor heb doorgenomen en verbeterd. De rest van de week stond in het teken van Angular oefeningen om de framework beter onder de knie te krijgen.',
     },
     {
       id: 2,
-      title: 'Tweede week van mijn stage',
+      title: 'Project setup en kennismaking met C# ASP.NET',
       date: new Date('2026-02-09'),
       content:
         'In de tweede week ben ik begonnen met de project setup en het opzetten van de connectie met GitLab. Daarna heb ik kennisgemaakt met C# ASP.NET door middel van oefeningen. Ik heb deze kennismakingsoefening verder aangepast en verbeterd, en ben blijven oefenen met C# ASP.NET. Ook heb ik een database schema aangemaakt. Aan het einde van de week heb ik een mockup ontworpen met v0.app.',
     },
     {
       id: 3,
-      title: 'Derde week van mijn stage',
+      title: 'Database schema en opzoekwerk naar authenticatiemethoden',
       date: new Date('2026-02-16'),
       content:
         'Deze week begon met een meeting waarin we de mockup en het database schema hebben besproken. Op basis van de feedback heb ik het database schema aangepast. Verder heb ik opzoekwerk gedaan naar verschillende authenticatiemethoden voor een multitenant applicatie, zoals OpenID en username/password.',
     },
     {
       id: 4,
-      title: 'Vierde week van mijn stage',
+      title: 'Opvolging database schema en blog applicatie',
       date: new Date('2026-02-23'),
       content:
         'De vierde week stond in het teken van het database schema en de blog applicatie. Na een meeting over het database schema ben ik begonnen aan de blog. Ik heb nagedacht over aanpassingen aan het database schema en heb de blog grotendeels afgemaakt.',
     },
     {
       id: 5,
-      title: 'Vijfde week van mijn stage',
+      title: 'Afmaken van database schema',
       date: new Date('2026-03-01'),
-      content: 'This is the content of my fifth blog post.',
+      content:
+        'We zijn de week begonnen met een meeting waar we weer de database schema hebben besproken, dankzij de hulp van Dimitri hebben we goede aanpassingen kunnen maken. Volgens de collegas zijn we ook heel dichtbij het eindresultaat.',
     },
     {
       id: 6,
